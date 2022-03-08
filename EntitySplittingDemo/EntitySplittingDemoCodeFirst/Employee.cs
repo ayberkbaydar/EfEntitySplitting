f0cc25894ff98dd16bba571f5fa3e214b067e2a8
Ayberk <ybrk01@gmail.com>
2022-03-08 11:44:56 +0300

    08-03
    08-03

---
+++ EntitySplittingDemo/EntitySplittingDemoCodeFirst/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EntitySplittingDemoCodeFirst
+{
+    public class Employee
+    {
+        //Employees
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+
+
+        //EmployeeContactDetails
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string LandLine { get; set; }
+
+    }
+}
---
+++ EntitySplittingDemo/EntitySplittingDemoCodeFirst/EmployeeDBContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntitySplittingDemoCodeFirst
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Map(map =>
+                {
+                    map.Properties(p => new
+                    {
+                        p.EmployeeId,
+                        p.FirstName,
+                        p.LastName,
+                        p.Gender
+                    });
+                    map.ToTable("Employees");
+                })
+                .Map(map =>
+                {
+                    map.Properties(p => new
+                    {
+                        p.EmployeeId,
+                        p.Email,
+                        p.Mobile,
+                        p.LandLine
+                    });
+                    map.ToTable("EmployeeContactDetails");
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
